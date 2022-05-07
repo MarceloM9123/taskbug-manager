265395e98030fb0f08f2224a9cebfbaa19db5fbd
marcelo <marcelomata91@gmail.com>
2022-05-07 16:22:02 -0500

    add navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import '../styles/globals.css';
 import { UserContext } from '../lib/context';
 import { Toaster } from 'react-hot-toast';
+import Navbar from '../components/navbar';
 
 
 function MyApp({ Component, pageProps }) {
   return( 
     <>
       <UserContext.Provider >
+        <NavBar />
         <Component {...pageProps} />
         <Toaster />
       </UserContext.Provider>
